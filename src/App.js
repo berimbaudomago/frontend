4cf8a30721f08658653a232a4af34b2efda03cba
Alexandre Fonseca <alexandre.fonseca25@gmail.com>
2022-10-09 23:40:34 -0300

    Adding "Services" to the structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import './App.css';
-import { useConnect, useDisconnect, useAccount, useNetwork } from "wagmi";
-
-
+import Visualizer from './services/visualizer.js';
 function App() {
-  const { data: accountData } = useAccount();
-  const { connect, connectors, isConnected } = useConnect();
-  const { disconnect } = useDisconnect();
-  const { activeChain } = useNetwork();
   
+
   return (
-    <div className="App">
-      <button onClick={() => { connect({ connector: connectors[0] }) }}>Coinbase Wallet</button>
-      <button onClick={() => { connect({ connector: connectors[1] }) }}>Wallet Connect</button>
-      <button onClick={() => { connect({ connector: connectors[2] }) }}>Metamask</button>
-
-      <button onClick={disconnect}>Disconnect</button>
-
-      <div>Connection Status: ${isConnected}</div>
-      <div>Account: ${accountData.address}</div>
-      <div>Network ID: ${activeChain.id}</div>
-    </div>
+    <Visualizer></Visualizer>
   );
 }
-
 
 export default App;
---
+++ src/services/visualizer.js
@@ -0,0 +1,24 @@
+import { useConnect, useDisconnect, useAccount, useNetwork } from "wagmi";
+
+
+class Visualizer extends Visualizer() {
+
+    //     const { data: accountData } = useAccount();
+    // const { connect, connectors, isConnected } = useConnect();
+    // const { disconnect } = useDisconnect();
+    // const { activeChain } = useNetwork();
+
+    // render(
+    //     //         <button onClick={() => { connect({ connector: connectors[0] }) }}>Coinbase Wallet</button>
+    //     //         <button onClick={() => { connect({ connector: connectors[1] }) }}>Wallet Connect</button>
+    //     //         <button onClick={() => { connect({ connector: connectors[2] }) }}>Metamask</button>
+
+    //     //         <button onClick={disconnect}>Disconnect</button>
+
+    //     //         <div>Connection Status: ${isConnected}</div>
+    //     //         <div>Account: ${accountData.address}</div>
+    //     //         <div>Network ID: ${activeChain.id}</div
+    // );
+}
+
+export default Visualizer; 
